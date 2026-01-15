0191798b2ec6f0fb6f373b935b44f022769b1db7
Jandavi <rodriguesjandavi193@gmail.com>
2026-01-15 18:35:22 -0300

    add obhect and details in new expense

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -21,5 +24,25 @@ function formatCurrencyBRL(value) {
   })
 
   return value
+}
+
+// Catura o evento de submit do formulário para obter o svalores
+form.onsubmit = (event) => {
+  // Previne comportamento padão de recarregar a página
+  event.preventDefault()
+
+  // Cria um objeto com detalhes na nova depesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value, 
+    create_at: new Date(),
+
+
+  }
+
+  console.log(newExpense)
 }
 
