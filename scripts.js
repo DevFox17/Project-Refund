a791f2a64dd3869981093e6b4ee749f8235aee70
Jandavi <rodriguesjandavi193@gmail.com>
2026-01-15 18:23:43 -0300

    formating value in standard BRL. add R$ in HTML

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount")
 
-
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
+  // Obtem o valor atual do input e remove os caracters não numéricos
   let value = amount.value.replace(/\D+/g, "")
-  
-  amount.value = value
 
+  // Transformar o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$ 1,50)
+  value = Number(value) / 100
+  
+  // Atualiza o valor do input
+  amount.value = formatCurrencyBRL(value)
 }
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL (Rea brasileiro)
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  return value
+}
+
