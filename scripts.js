ef5fe369ea65448960fcba38ab7224fa49d5f08d
Jandavi <rodriguesjandavi193@gmail.com>
2026-01-16 14:15:17 -0300

    add item create by user on the  JS for show on the screen

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
+// Seleciona os elementos da lista
+const expenseList = document.querySelector("ul")
+
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
   // Obtem o valor atual do input e remove os caracters não numéricos
@@ -11,7 +14,7 @@ amount.oninput = () => {
 
   // Transformar o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$ 1,50)
   value = Number(value) / 100
-  
+
   // Atualiza o valor do input
   amount.value = formatCurrencyBRL(value)
 }
@@ -37,7 +40,7 @@ form.onsubmit = (event) => {
     expense: expense.value,
     category_id: category.value,
     category_name: category.options[category.selectedIndex].text,
-    amount: amount.value, 
+    amount: amount.value,
     create_at: new Date(),
 
   }
@@ -48,9 +51,37 @@ form.onsubmit = (event) => {
 
 function expenseAdd(newExpense) {
   try {
-    
+    // Cria um elemento de li para adicionar o item (li) na lista (ul)
+    const expenseItem = document.createElement("li")
+    expenseItem.classList.add("expense")
+
+    // Cria o ícone da categoria
+    const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    // Cria a info da dispesa
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    // Cria o nome da despesa
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    // Cira a categoria da despesa
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    // Adiciona name e category na div das informações da dispesa
+    expenseInfo.append(expenseName, expenseCategory)
+
+    // Adiciona as informações no item
+    expenseItem.append(expenseIcon, expenseInfo)
+
+    // Aduciona o item na lista
+    expenseList.append(expenseItem)
   } catch (error) {
-    alert("Não foi possível atualizar a lisat de dispesa")
+    alert("Não foi possível atualizar a lista de dispesa")
     console.log(error)
   }
 }
