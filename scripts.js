52008bd7dd8a005b01fda4bd0cd00ca85435ebe9
Jandavi <rodriguesjandavi193@gmail.com>
2026-01-16 15:57:47 -0300

    adding interaction to remove item from the list

--- scripts.js
+++ scripts.js
@@ -154,4 +154,18 @@ function updateTotals () {
   }
 }
 
+// Evento que captura clique nos itens da lista
+expenseList.addEventListener("click", function (event){
+  // Verificar se o elemento clicado é o ícone de remover
+  if(event.target.classList.contains("remove-icon")) {
+    // Obtem a li pai do elemento clicado
+    const item = event.target.closest(".expense")
+     // Remove o item da lista 
+    item.remove()
+  }
+
+ // Atualiza os totais
+  updateTotals()
+})
+
 
